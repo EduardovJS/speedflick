9718083229238f7644a6f674a40d5ba092e43309
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-03-31 21:28:40 -0300

    metodos para autenticar, raking, atualizar raking

--- backend/SpeedFlick/SpeedFlick/Controllers/GameController.cs
+++ backend/SpeedFlick/SpeedFlick/Controllers/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SpeedFlick.Context;
@@ -17,12 +18,14 @@ namespace SpeedFlick.Controllers
             _context = context;
         }
 
+        // Inicia a autenticação com Discord
         [HttpGet("login")]
         public IActionResult Login()
         {
             return Challenge(new AuthenticationProperties { RedirectUri = "/api/jogo/autenticar" }, "Discord");
         }
 
+        // Autenticação e criação do usuário no banco de dados
         [HttpGet("autenticar")]
         public IActionResult Callback()
         {
@@ -48,6 +51,41 @@ namespace SpeedFlick.Controllers
                 AvatarUrl = avatarUrl
             };
             return Json(userProfile); // Redireciona para o método Conta()
+        }
+
+        // Verifica se o usuário está autenticado
+        [HttpGet("verify")]
+        public IActionResult Verify()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Ok();
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        // Atualiza a pontuação do usuário
+        [HttpPost("update-score")]
+        public IActionResult UpdateScore([FromBody] UserScore userScore)
+        {
+            var existingUser = _context.UserScores.FirstOrDefault(u => u.UserId == userScore.UserId);
+            if (existingUser != null)
+            {
+                existingUser.Score = userScore.Score;
+                _context.SaveChanges();
+            }
+            return Ok();
+        }
+
+        // Obtém o ranking dos jogadores
+        [HttpGet("ranking")]
+        public IActionResult GetRanking()
+        {
+            var ranking = _context.UserScores.OrderByDescending(u => u.Score).Take(10).ToList();
+            return Ok(ranking);
         }
     }
 }
